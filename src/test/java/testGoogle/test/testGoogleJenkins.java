223c46167d30c51001a3d9bbbbc70adb13d0e4e1
thi-ly.tran <thi-ly.tran@ekino.com>
2018-06-27 18:30:11 +0700

    add new class

---
+++ src/test/java/testGoogle/test/testGoogleJenkins.java
@@ -0,0 +1,15 @@
+package testGoogle.test;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class testGoogleJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RemoteWebDriver driver = new FirefoxDriver();
+		driver.get("www.google.com");
+		driver.quit();
+	}
+
+}
